b76aa746c131856ddcba773017535385838ed622
2634y <2634y@DESKTOP-F5I25NL>
2020-07-15 16:17:18 +0900

    ADD SCHEDULER FUNCTIONS
    add scheduler controller
    add scheduler service and dao
    connect submenubar with groupCalendar.jsp

---
+++ src/com/kh/groumoa/scheduler/model/dao/SchedulerDao.java
@@ -0,0 +1,14 @@
+package com.kh.groumoa.scheduler.model.dao;
+
+import java.sql.Connection;
+
+import com.kh.groumoa.scheduler.model.vo.SchedulerVO;
+
+public class SchedulerDao {
+
+	public SchedulerVO ScheduleInsert(Connection con, SchedulerVO reqestMember) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/com/kh/groumoa/scheduler/model/vo/SchedulerVO.java
@@ -0,0 +1,104 @@
+package com.kh.groumoa.scheduler.model.vo;
+
+import java.sql.Date;
+
+public class SchedulerVO {
+	
+	private String schCode;
+	private String schTitle;
+	private String schDetail;
+	private Date schDate;
+	private Date dateWritten;
+	private String groupCode;
+	private int writerCode;
+	private char status;
+	
+	public SchedulerVO() {
+		super();
+	}
+
+	public SchedulerVO(String schCode, String schTitle, String schDetail, Date schDate, Date dateWritten,
+			String groupCode, int writerCode, char status) {
+		super();
+		this.schCode = schCode;
+		this.schTitle = schTitle;
+		this.schDetail = schDetail;
+		this.schDate = schDate;
+		this.dateWritten = dateWritten;
+		this.groupCode = groupCode;
+		this.writerCode = writerCode;
+		this.status = status;
+	}
+
+	public String getSchCode() {
+		return schCode;
+	}
+
+	public void setSchCode(String schCode) {
+		this.schCode = schCode;
+	}
+
+	public String getSchTitle() {
+		return schTitle;
+	}
+
+	public void setSchTitle(String schTitle) {
+		this.schTitle = schTitle;
+	}
+
+	public String getSchDetail() {
+		return schDetail;
+	}
+
+	public void setSchDetail(String schDetail) {
+		this.schDetail = schDetail;
+	}
+
+	public Date getSchDate() {
+		return schDate;
+	}
+
+	public void setSchDate(Date schDate) {
+		this.schDate = schDate;
+	}
+
+	public Date getDateWritten() {
+		return dateWritten;
+	}
+
+	public void setDateWritten(Date dateWritten) {
+		this.dateWritten = dateWritten;
+	}
+
+	public String getGroupCode() {
+		return groupCode;
+	}
+
+	public void setGroupCode(String groupCode) {
+		this.groupCode = groupCode;
+	}
+
+	public int getWriterCode() {
+		return writerCode;
+	}
+
+	public void setWriterCode(int writerCode) {
+		this.writerCode = writerCode;
+	}
+
+	public char getStatus() {
+		return status;
+	}
+
+	public void setStatus(char status) {
+		this.status = status;
+	}
+
+	@Override
+	public String toString() {
+		return "ScheduleVO [schCode=" + schCode + ", schTitle=" + schTitle + ", schDetail=" + schDetail + ", schDate="
+				+ schDate + ", dateWritten=" + dateWritten + ", groupCode=" + groupCode + ", writerCode=" + writerCode
+				+ ", status=" + status + "]";
+	}
+	
+}
