813101e06cb19e2cd661c8d0ab7cc92260b6aaf4
tmddu <tmddu@DESKTOP-RSHJIHM>
2020-07-27 21:48:02 +0900

    고객세넡 메인
    ㅇㅇ

--- src/com/kh/groumoa/inquire/controller/SelectInquireListServlet.java
+++ src/com/kh/groumoa/inquire/controller/SelectInquireListServlet.java
@@ -1,5 +1,4 @@
 package com.kh.groumoa.inquire.controller;
-
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -10,12 +9,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.kh.groumoa.common.PageInfo;
-import com.kh.groumoa.group.model.service.BoardService;
-import com.kh.groumoa.group.model.vo.BoardVO;
-import com.kh.groumoa.group.model.vo.GroupVO;
 import com.kh.groumoa.inquire.model.service.InquireService;
 import com.kh.groumoa.inquire.model.vo.InquireVO;
-import com.kh.groumoa.member.model.vo.MemberVO;
+
 
 /**
  * Servlet implementation class SelectInquireListServlet
@@ -36,10 +32,17 @@ public class SelectInquireListServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ArrayList<InquireVO> selectQna = new InquireService().selectInquire();
+		System.out.println("selectQna : " + selectQna);
+		String page = "";
 		
+		if(selectQna != null) {
+			page = "/views/group/myInquiry.jsp";
+			request.setAttribute("selectQna", selectQna);
+			request.getRequestDispatcher(page).forward(request, response);
+		}
+
 	}
-
-
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
